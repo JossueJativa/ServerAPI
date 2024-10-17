c16083f261208467f4154c10593bd45fbbfd7cb9
Jossue <jativatamayojossuedavid@gmail.com>
2024-10-17 10:59:04 -0500

    Agregar al admin todos los modelos

--- server/API/admin.py
+++ server/API/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, Area, Home, Home_User, Security, Security_Area
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Area)
+admin.site.register(Home)
+admin.site.register(Home_User)
+admin.site.register(Security)
+admin.site.register(Security_Area)
