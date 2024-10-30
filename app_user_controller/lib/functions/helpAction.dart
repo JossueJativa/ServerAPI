7a48149dce402602532af7f3517e32bc2651221e
Jossue <jativatamayojossuedavid@gmail.com>
2024-10-30 11:11:06 -0500

    Llega mensaje si o si

--- app_user_controller/lib/functions/helpAction.dart
+++ app_user_controller/lib/functions/helpAction.dart
@@ -4,14 +4,21 @@ import 'package:http/http.dart' as http;
 Future<bool> helpAction(int homeId) async {
   final Map<String, dynamic> jsonAuth = await loadAuthData();
   final String url = jsonAuth['url'] + '/area/HelpBtn/?home_id=$homeId';
+  try{
+    final response = await http.get(
+      Uri.parse(url),
+    );
 
-  final response = await http.get(
-    Uri.parse(url),
-  );
-
-  if (response.statusCode == 200) {
-    return true;
-  } else {
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  } 
+  catch (e) {
+    return false;
+  }
+  finally {
     return false;
   }
 }
